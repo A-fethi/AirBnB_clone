f64ac9f1b697cc822229556e2ebf7f7219e1e3a5
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-07 09:44:55 -0700

    fix updated_at and make it work with *args and **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """Represents the BaseModel of HBnB project."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new instance of the BaseModel class.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        forma = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    self.__dict__[key] = datetime.strptime(value, forma)
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -24,7 +32,7 @@ class BaseModel:
         """
         Update the updated_at attribute with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -37,5 +45,5 @@ class BaseModel:
         dicts = self.__dict__.copy()
         dicts["__class__"] = self.__class__.__name__
         dicts["created_at"] = self.created_at.isoformat()
-        dicts["updated_at"] = self.created_at.isoformat()
+        dicts["updated_at"] = self.updated_at.isoformat()
         return dicts
