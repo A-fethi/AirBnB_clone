5ada574e302743177d322d3f1510525a7413b247
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-08 06:50:00 -0700

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defines the BaseModel class."""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -33,6 +35,8 @@ class BaseModel:
         Update the updated_at attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
+
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,40 @@
 #!usr/bin/python3
+"""Defines the FileStorage class."""
+from models.base_model import BaseModel
 import json
 
 
 class FileStorage:
+    """Represents an abstracted storage engine."""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Return the dict __objects."""
         return self.__objects
 
     def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
+        """Serialize __objects to the JSON file."""
+        data = {}
         for key, value in self.__objects.items():
             data[key] = value.to_dict()
-        with open(self.__file_path, "w") as file:
-            json.dump(data, file)
+        with open(self.__file_path, "w") as f:
+            json.dump(data, f)
+
+    def reload(self):
+        """Desrialize the JSON file to __objects"""
+        try:
+            with open(self.__file_path, "r") as f:
+                objects = json.load(f)
+                for obj_data in objects.values():
+                    name = obj_data["__class__"]
+                    del obj_data["__class__"]
+                    self.new(eval(name)(**obj_data))
+        except FileNotFoundError:
+            pass
